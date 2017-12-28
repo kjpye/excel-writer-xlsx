8853d575a1b8f5a0637a81a15e680e763630d14c
Kevin Pye <admin-Kevin.Pye@cenitex.vic.gov.au>
2017-12-28 11:40:52 +1100

    First pass at Comments.pm

--- lib/Excel/Writer/XLSX/Package/Comments.pm
+++ lib/Excel/Writer/XLSX/Package/Comments.pm
@@ -1,4 +1,4 @@
-package Excel::Writer::XLSX::Package::Comments;
+unit class Excel::Writer::XLSX::Package::Comments;
 
 ###############################################################################
 #
@@ -13,16 +13,13 @@ package Excel::Writer::XLSX::Package::Comments;
 
 # perltidy with the following options: -mbl=2 -pt=0 -nola
 
-use 5.008002;
-use strict;
-use warnings;
-use Carp;
-use Excel::Writer::XLSX::Package::XMLwriter;
-use Excel::Writer::XLSX::Utility qw(xl_rowcol_to_cell);
+use v6.c;
+#use Excel::Writer::XLSX::Package::XMLwriter;
+#use Excel::Writer::XLSX::Utility qw(xl_rowcol_to_cell);
 
 
-our @ISA     = qw(Excel::Writer::XLSX::Package::XMLwriter);
-our $VERSION = '0.96';
+#NYI our @ISA     = qw(Excel::Writer::XLSX::Package::XMLwriter);
+#NYI our $VERSION = '0.96';
 
 
 ###############################################################################
@@ -31,6 +28,7 @@ our $VERSION = '0.96';
 #
 ###############################################################################
 
+has %!author_ids;
 
 ###############################################################################
 #
@@ -38,18 +36,18 @@ our $VERSION = '0.96';
 #
 # Constructor.
 #
-sub new {
+#NYI sub new {
 
-    my $class = shift;
-    my $fh    = shift;
-    my $self  = Excel::Writer::XLSX::Package::XMLwriter->new( $fh );
+#NYI     my $class = shift;
+#NYI     my $fh    = shift;
+#NYI     my $self  = Excel::Writer::XLSX::Package::XMLwriter->new( $fh );
 
-    $self->{_author_ids} = {};
+#NYI     $self->{_author_ids} = {};
 
-    bless $self, $class;
+#NYI     bless $self, $class;
 
-    return $self;
-}
+#NYI     return $self;
+#NYI }
 
 
 ###############################################################################
@@ -58,26 +56,22 @@ sub new {
 #
 # Assemble and write the XML file.
 #
-sub _assemble_xml_file {
-
-    my $self          = shift;
-    my $comments_data = shift;
-
-    $self->xml_declaration;
+method assemble_xml_file($comments-data) {
+    self.xml_declaration;
 
     # Write the comments element.
-    $self->_write_comments();
+    self.write_comments();
 
     # Write the authors element.
-    $self->_write_authors( $comments_data );
+    self.write_authors( $comments-data );
 
     # Write the commentList element.
-    $self->_write_comment_list( $comments_data );
+    self.write_comment_list( $comments-data );
 
-    $self->xml_end_tag( 'comments' );
+    self.xml_end_tag( 'comments' );
 
     # Close the XML writer filehandle.
-    $self->xml_get_fh()->close();
+    self.xml_get_fh.close();
 }
 
 
@@ -101,14 +95,12 @@ sub _assemble_xml_file {
 #
 # Write the <comments> element.
 #
-sub _write_comments {
-
-    my $self  = shift;
+method write_comments {
     my $xmlns = 'http://schemas.openxmlformats.org/spreadsheetml/2006/main';
 
     my @attributes = ( 'xmlns' => $xmlns );
 
-    $self->xml_start_tag( 'comments', @attributes );
+    self.xml_start_tag( 'comments', @attributes );
 }
 
 
@@ -118,28 +110,25 @@ sub _write_comments {
 #
 # Write the <authors> element.
 #
-sub _write_authors {
-
-    my $self         = shift;
-    my $comment_data = shift;
+method write_authors($comment-data) {
     my $author_count = 0;
 
-    $self->xml_start_tag( 'authors' );
+    self.xml_start_tag( 'authors' );
 
-    for my $comment ( @$comment_data ) {
-        my $author = $comment->[3];
+    for $comment-data -> $comment {
+        my $author = $comment[3];
 
-        if ( defined $author && !exists $self->{_author_ids}->{$author} ) {
+        if $author.defined && ! %!author_ids{$author}.exists {
 
             # Store the author id.
-            $self->{_author_ids}->{$author} = $author_count++;
+            %!author_ids{$author} = $author_count++;
 
             # Write the author element.
-            $self->_write_author( $author );
+            self.write_author( $author );
         }
     }
 
-    $self->xml_end_tag( 'authors' );
+    self.xml_end_tag( 'authors' );
 }
 
 
@@ -149,12 +138,8 @@ sub _write_authors {
 #
 # Write the <author> element.
 #
-sub _write_author {
-
-    my $self = shift;
-    my $data = shift;
-
-    $self->xml_data_element( 'author', $data );
+method write_author($data) {
+    self.xml_data_element( 'author', $data );
 }
 
 
@@ -164,28 +149,24 @@ sub _write_author {
 #
 # Write the <commentList> element.
 #
-sub _write_comment_list {
+method write_comment_list($comment-data) {
+    self.xml_start_tag( 'commentList' );
 
-    my $self         = shift;
-    my $comment_data = shift;
-
-    $self->xml_start_tag( 'commentList' );
-
-    for my $comment ( @$comment_data ) {
-        my $row    = $comment->[0];
-        my $col    = $comment->[1];
-        my $text   = $comment->[2];
-        my $author = $comment->[3];
+    for $comment-data -> $comment {
+        my $row    = $comment[0];
+        my $col    = $comment[1];
+        my $text   = $comment[2];
+        my $author = $comment[3];
 
         # Look up the author id.
-        my $author_id = undef;
-        $author_id = $self->{_author_ids}->{$author} if defined $author;
+        my $author-id;
+        $author-id = %!author_ids{$author} if defined $author;
 
         # Write the comment element.
-        $self->_write_comment( $row, $col, $text, $author_id );
+        self.write_comment( $row, $col, $text, $author-id );
     }
 
-    $self->xml_end_tag( 'commentList' );
+    self.xml_end_tag( 'commentList' );
 }
 
 
@@ -195,27 +176,21 @@ sub _write_comment_list {
 #
 # Write the <comment> element.
 #
-sub _write_comment {
-
-    my $self      = shift;
-    my $row       = shift;
-    my $col       = shift;
-    my $text      = shift;
-    my $author_id = shift;
+method write_comment($row, $col, $text, $author-id) {
     my $ref       = xl_rowcol_to_cell( $row, $col );
 
     my @attributes = ( 'ref' => $ref );
 
-    push @attributes, ( 'authorId' => $author_id ) if defined $author_id;
+    @attributes.push: ( 'authorId' => $author-id ) if defined $author-id;
 
 
-    $self->xml_start_tag( 'comment', @attributes );
+    self.xml_start_tag( 'comment', @attributes );
 
     # Write the text element.
-    $self->_write_text( $text );
+    self.write_text( $text );
 
 
-    $self->xml_end_tag( 'comment' );
+    self.xml_end_tag( 'comment' );
 }
 
 
@@ -225,17 +200,13 @@ sub _write_comment {
 #
 # Write the <text> element.
 #
-sub _write_text {
-
-    my $self = shift;
-    my $text = shift;
-
-    $self->xml_start_tag( 'text' );
+method write_text($text) {
+    self.xml_start_tag( 'text' );
 
     # Write the text r element.
-    $self->_write_text_r( $text );
+    self.write_text_r( $text );
 
-    $self->xml_end_tag( 'text' );
+    self.xml_end_tag( 'text' );
 }
 
 
@@ -245,20 +216,16 @@ sub _write_text {
 #
 # Write the <r> element.
 #
-sub _write_text_r {
-
-    my $self = shift;
-    my $text = shift;
-
-    $self->xml_start_tag( 'r' );
+method write_text_r($text) {
+    self.xml_start_tag( 'r' );
 
     # Write the rPr element.
-    $self->_write_r_pr();
+    self.write_r_pr();
 
     # Write the text r element.
-    $self->_write_text_t( $text );
+    self.write_text_t( $text );
 
-    $self->xml_end_tag( 'r' );
+    self.xml_end_tag( 'r' );
 }
 
 
@@ -268,18 +235,14 @@ sub _write_text_r {
 #
 # Write the text <t> element.
 #
-sub _write_text_t {
-
-    my $self = shift;
-    my $text = shift;
-
+method write_text_t($text) {
     my @attributes = ();
 
-    if ( $text =~ /^\s/ || $text =~ /\s$/ ) {
+    if $text ~~ /^\s/ || $text ~~ /\s$/ {
         push @attributes, ( 'xml:space' => 'preserve' );
     }
 
-    $self->xml_data_element( 't', $text, @attributes );
+    self.xml_data_element( 't', $text, @attributes );
 }
 
 
@@ -289,25 +252,22 @@ sub _write_text_t {
 #
 # Write the <rPr> element.
 #
-sub _write_r_pr {
-
-    my $self = shift;
-
-    $self->xml_start_tag( 'rPr' );
+method write_r_pr {
+    self.xml_start_tag( 'rPr' );
 
     # Write the sz element.
-    $self->_write_sz();
+    self.write_sz();
 
     # Write the color element.
-    $self->_write_color();
+    self.write_color();
 
     # Write the rFont element.
-    $self->_write_r_font();
+    self.write_r_font();
 
     # Write the family element.
-    $self->_write_family();
+    self.write_family();
 
-    $self->xml_end_tag( 'rPr' );
+    self.xml_end_tag( 'rPr' );
 }
 
 
@@ -317,14 +277,12 @@ sub _write_r_pr {
 #
 # Write the <sz> element.
 #
-sub _write_sz {
-
-    my $self = shift;
+method write_sz {
     my $val  = 8;
 
     my @attributes = ( 'val' => $val );
 
-    $self->xml_empty_tag( 'sz', @attributes );
+    self.xml_empty_tag( 'sz', @attributes );
 }
 
 
@@ -334,14 +292,12 @@ sub _write_sz {
 #
 # Write the <color> element.
 #
-sub _write_color {
-
-    my $self    = shift;
+method write_color {
     my $indexed = 81;
 
     my @attributes = ( 'indexed' => $indexed );
 
-    $self->xml_empty_tag( 'color', @attributes );
+    self.xml_empty_tag( 'color', @attributes );
 }
 
 
@@ -351,14 +307,12 @@ sub _write_color {
 #
 # Write the <rFont> element.
 #
-sub _write_r_font {
-
-    my $self = shift;
+method write_r_font {
     my $val  = 'Tahoma';
 
     my @attributes = ( 'val' => $val );
 
-    $self->xml_empty_tag( 'rFont', @attributes );
+    self.xml_empty_tag( 'rFont', @attributes );
 }
 
 
@@ -368,18 +322,16 @@ sub _write_r_font {
 #
 # Write the <family> element.
 #
-sub _write_family {
-
-    my $self = shift;
+method write_family {
     my $val  = 2;
 
     my @attributes = ( 'val' => $val );
 
-    $self->xml_empty_tag( 'family', @attributes );
+    self.xml_empty_tag( 'family', @attributes );
 }
 
 
-1;
+=begin pod
 
 
 __END__
@@ -417,3 +369,4 @@ Either the Perl Artistic Licence L<http://dev.perl.org/licenses/artistic.html> o
 See the documentation for L<Excel::Writer::XLSX>.
 
 =cut
+=end pod
